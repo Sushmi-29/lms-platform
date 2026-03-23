79479b3656a353837a536f756aa3ecedef9dd3b7
Sushmitha Ganiga <ganigasushmitha28@gmail.com>
2026-03-23 11:12:13 +0530

    fix render port properly

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,10 +4,10 @@ dotenv.config();
 import app from './app';
 import { connectDB } from './config/db';
 
-const PORT = Number(process.env.PORT) || 5000;
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
